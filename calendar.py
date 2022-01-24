6831cb3b1f73e92de364d8a8cdcdab70d1a8b33e
NV <69376697+Nandeshwar-Verma@users.noreply.github.com>
2022-01-24 10:22:28 -0500

    calendar

---
+++ calendar.py
@@ -0,0 +1,14 @@
+import calendar
+"""
+c=calendar.HTMLcalendar(calendar.SUNDAY)
+st=c.formatmonth(2017,1)
+print(st)
+"""
+
+
+for name in calendar.month_name:
+    print(name)
+
+
+for day_name in calendar.day_name:
+    print(day_name)
